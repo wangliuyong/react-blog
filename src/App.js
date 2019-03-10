bf026752b40b82859d28e89944fcd7fc52f0b44e
wangliuyong <1355498705@qq.com>
2019-03-10 15:41:16 +0800

    install axios and message component

--- src/App.js
+++ src/App.js
@@ -4,27 +4,18 @@ import { Button ,message} from 'antd';
 
 import './App.css';
 
-/*practive 专用*/
-
-import {Welcome} from './practive/Test'
-import {Tick} from './practive/Tick'
-
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <Button type="primary" onClick={this.handleClick}>Button</Button>
         </header>
-        <Welcome name='wangliuyong'/>
-        <Tick />
       </div>
     );
   }
 
   handleClick(){
-    console.log('按钮被点击了');
     message.error('error')
   }
 }
