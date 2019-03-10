23732b99fa833eb0f58d5a8d88dae7faaa6f7394
wangliuyong <1355498705@qq.com>
2019-03-10 12:13:57 +0800

    测试ant-design的message组件

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 
-import { Button } from 'antd';
+import { Button ,message} from 'antd';
 
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Button type="primary">Button</Button>
+          <Button type="primary" onClick={this.handleClick}>Button</Button>
         </header>
       </div>
     );
+  }
+
+  handleClick(){
+    console.log('按钮被点击了');
+    message.error('error')
   }
 }
 
